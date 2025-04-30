21018a67b9df7f2b780ddc78fc8032421a0e626a
avipatel <avipatel@buildformula.com>
2025-04-30 17:55:57 +0530

    .

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
 
   const validate = () => {
     const newErrors = {};
-    if (!inTime) newErrors.inTime = "Required";
+    if (!inTime) newErrors.inTime = "Required in time";
     if (!breakStart) newErrors.breakStart = "Required";
     if (!breakEnd) newErrors.breakEnd = "Required";
     setErrors(newErrors);
